21f67a6749eebae4b60dfd511c728007a007a8c4
Eric Suchyta <eric.d.suchyta@gmail.com>
2015-02-01 08:22:01 -0800

    NerscConfigure.py file added

---
+++ NerscConfigure.py
@@ -0,0 +1,53 @@
+import RunConfigurations
+import os
+import esutil
+
+def GetConfig():
+
+    # These effect a whole run's behavior. That is they are higher level than a single Balrog call.
+    run = RunConfigurations.RunConfigurations.default
+    run['label'] = 'debug-nersc'
+    run['tiletotal'] = 1000
+    run['DBoverwrite'] = True
+    run['DBload'] = 'cx_Oracle'
+    run['bands'] = ['i']
+    run['dualdetection'] = None
+
+    #run['funpack'] = '/astro/u/esuchyta/cfitsio/cfitsio-3.370-install/bin/funpack'
+    run['swarp'] = '/global/project/projectdirs/des/wl/desdata/users/esuchyta/software/carver/swarp-2.36.1/install-dir/bin/swarp'
+    run['outdir']: os.path.join(os.environ['SCRATCH'], 'BalrogOutput')  # The ouput directory for all intermediate work. This should be in the scratch area on the node.
+
+
+    # These get passes as command line arguments to Balrog. If you add too much it could mess things up.
+    balrog = RunConfigurations.BalrogConfigurations.default
+    #balrog['sexparam'] = os.path.join(os.environ['BALROG_MPI_ASTRO_CONFIG'], 'sva1', 'sex.param')
+    run['pyconfig'] = os.path.join(os.environ['BALROG_MPI'], 'pyconfig', 'slr2.py'),
+    run['sexnnw'] = os.path.join(os.environ['BALROG_MPI'], 'astro_config', 'sva1', 'sex.nnw'),
+    run['sexconv'] = os.path.join(os.environ['BALROG_MPI'], 'astro_config', 'sva1', 'sex.conv'),
+    run['sexparam'] = os.path.join(os.environ['BALROG_MPI'], 'astro_config', 'sva1', 'sex.param_diskonly'),
+    run['sexconfig'] = os.path.join(os.environ['BALROG_MPI'], 'astro_config', 'sva1', 'sex.config'),
+
+    run['sexpath'] = '/direct/astro+u/esuchyta/svn_repos/sextractor-2.18.10/install/bin/sex',
+
+
+    # This is configuring desdb to find the right files.
+    DESdb = RunConfigurations.desdbInfo.sva1_coadd
+
+
+    # Info for connecting to the DB. You probably don't need to touch this.
+    db = RunConfigurations.DBInfo.default
+
+
+    # What tiles you want to Balrog
+    tiles = RunConfigurations.TileLists.suchyta13
+    #tileinfo = esutil.io.read('spte-tiles.fits')
+    #tiles = tileinfo['tilename'][0:30]
+
+
+    #tiles = cur.quick("SELECT tile.tilename, tile.urall, tile.uraur, tile.udecll, tile.udecur from coaddtile tile   JOIN (select distinct(tilename) as tilename from sva1_coadd_spte) sva1 ON sva1.tilename=tile.tilename  ORDER BY tile.udecll DESC, tile.urall ASC", array=True)
+
+    return run, balrog, DESdb, db, tiles
+
+
+# Runs
+# 0-30
