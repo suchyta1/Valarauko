58b08d49bcb820cff696adac6a089df34bfd84ec
Eric Suchyta <eric.d.suchyta@gmail.com>
2015-02-10 11:24:56 -0800

    adding NERSC config file version

---
+++ NERSCCustomConfig.py
@@ -0,0 +1,42 @@
+import os
+
+# change the defaults if you want
+
+def CustomConfig(run, balrog, DESdb, db, tiles, where):
+    #run['label'] = 'debug_bnl'
+    #run['label'] = 'dbg_c'
+    #run['DBload'] = 'cx_Oracle'
+
+    #run['tiletotal'] = 100000
+    run['tiletotal'] = 7015
+    run['DBoverwrite'] = True
+    #run['DBload'] = 'sqlldr'
+    run['DBload'] = 'cx_Oracle'
+    run['inc'] = 10
+    run['outdir'] = os.path.join(os.environ['SCRATCH'], 'BalrogScratch')
+    run['intermediate-clean'] = True
+    run['tile-clean'] = True
+
+    run['bands'] = ['r', 'i', 'z']
+    run['dualdetection'] = [1,2,3]
+
+    balrog['oldmorph'] = True
+    if balrog['oldmorph']:
+        balrog["reff"] = "HALF_LIGHT_RADIUS"
+        balrog["sersicindex"] = "SERSIC_INDEX"
+
+    #run['label'] = 'sva1_test'
+    #run['joblabel'] = 'test'
+
+    run['label'] = 'ndbg'
+    run['joblabel'] = 'test'
+    run['ppn'] = 8
+    run['walltime'] = '01:00:00'
+
+    #run['nodes'] = 10
+    #tiles = tiles[0:30]
+
+    run['nodes'] = 2
+    tiles = tiles[0:2]
+
+    return run, balrog, DESdb, db, tiles
