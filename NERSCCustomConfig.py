11a3fe1deb7028a54edaaf0f165fba4fac0f66ae
suchyta1 <eric.d.suchyta@gmail.com>
2015-02-12 14:45:47 -0500

    one more small debug

--- NERSCCustomConfig.py
+++ NERSCCustomConfig.py
@@ -8,15 +8,15 @@ def CustomConfig(run, balrog, DESdb, db, tiles, where):
     run['label'] = 'ndbg'
     run['joblabel'] = 'test'
     run['ppn'] = 8
-    run['nodes'] = 10
+    run['nodes'] = 2
     run['walltime'] = '24:00:00'
     run['queue'] = 'regular'
-    tiles = tiles[30:40]
+    tiles = tiles[30:32]
 
 
     # If you're not debugging these should be pretty stable not to need to change. 100,000 for the tiletotal gets you to about observed DES number density.
     # Warning: if you make the cleaning parameters False you will use LOTS of disk space
-    run['tiletotal'] = 50000
+    run['tiletotal'] = 5000
     run['DBoverwrite'] = True
     run['command'] = 'system'
     run['DBload'] = 'cx_Oracle'
