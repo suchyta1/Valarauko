7c1739c3f89a580f9fdf7f63cc58b7f4168d66d1
suchyta1 <eric.d.suchyta@gmail.com>
2015-02-12 12:13:25 -0500

    doing exactly the same thing at BNL

--- BNLCustomConfig.py
+++ BNLCustomConfig.py
@@ -33,6 +33,7 @@ def CustomConfig(run, balrog, DESdb, db, tiles, where):
     '''
 
 
+    '''
     run['label'] = 'debug'
     run['joblabel'] = 'test'
     run['ppn'] = 6
@@ -54,5 +55,38 @@ def CustomConfig(run, balrog, DESdb, db, tiles, where):
     if balrog['oldmorph']:
         balrog["reff"] = "HALF_LIGHT_RADIUS"
         balrog["sersicindex"] = "SERSIC_INDEX"
+    '''
+
+    # Always check these
+    run['label'] = 'ndbg'
+    run['joblabel'] = 'test'
+    run['ppn'] = 6
+    run['nodes'] = 2
+    run['walltime'] = '24:00:00'
+    run['queue'] = 'regular'
+    tiles = tiles[30:32]
+
+
+    # If you're not debugging these should be pretty stable not to need to change. 100,000 for the tiletotal gets you to about observed DES number density.
+    # Warning: if you make the cleaning parameters False you will use LOTS of disk space
+    run['tiletotal'] = 5000
+    run['DBoverwrite'] = True
+    run['command'] = 'system'
+    run['DBload'] = 'cx_Oracle'
+    run['inc'] = 100
+    run['outdir'] = os.path.join(os.environ['SCRATCH'], 'BalrogScratch')
+    run['intermediate-clean'] = True
+    run['tile-clean'] = True
+    run['bands'] = ['g', 'r', 'i', 'z', 'Y']
+    run['dualdetection'] = [1,2,3]
+    balrog['oldmorph'] = True
+
+
+    if balrog['oldmorph']:
+        balrog["reff"] = "HALF_LIGHT_RADIUS"
+        balrog["sersicindex"] = "SERSIC_INDEX"
+
+
+    print 'is anything even happening?'
 
     return run, balrog, DESdb, db, tiles
--- NERSCCustomConfig.py
+++ NERSCCustomConfig.py
@@ -7,16 +7,16 @@ def CustomConfig(run, balrog, DESdb, db, tiles, where):
     # Always check these
     run['label'] = 'ndbg'
     run['joblabel'] = 'test'
-    run['ppn'] = 8
-    run['nodes'] = 10
+    run['ppn'] = 6
+    run['nodes'] = 2
     run['walltime'] = '24:00:00'
     run['queue'] = 'regular'
-    tiles = tiles[30:40]
+    tiles = tiles[30:32]
 
 
     # If you're not debugging these should be pretty stable not to need to change. 100,000 for the tiletotal gets you to about observed DES number density.
     # Warning: if you make the cleaning parameters False you will use LOTS of disk space
-    run['tiletotal'] = 50000
+    run['tiletotal'] = 5000
     run['DBoverwrite'] = True
     run['command'] = 'system'
     run['DBload'] = 'cx_Oracle'
@@ -26,7 +26,7 @@ def CustomConfig(run, balrog, DESdb, db, tiles, where):
     run['tile-clean'] = True
     run['bands'] = ['g', 'r', 'i', 'z', 'Y']
     run['dualdetection'] = [1,2,3]
-    balrog['oldmorph'] = False
+    balrog['oldmorph'] = True
 
 
     if balrog['oldmorph']:
