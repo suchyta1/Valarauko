b9b3be483be1cf75bc6b584d5da73434c788fa5c
suchyta1 <eric.d.suchyta@gmail.com>
2015-12-01 15:38:03 -0500

    use regular queue

--- NERSCCustomConfig.py
+++ NERSCCustomConfig.py
@@ -49,8 +49,8 @@ def CustomConfig(run, balrog, db, tiles):
     tiles = tiles[50:52]
     run['ppn'] = 32
     run['nodes'] = 2
-    run['walltime'] = '00:30:00'
-    run['queue'] = 'debug'
+    run['walltime'] = '24:00:00'
+    run['queue'] = 'regular'
 
     run['label'] = 'y1_test'
     run['joblabel'] = '50-52'
