039ce92bc09a05852a556b666c604b8303f935ec
Eric Suchyta <eric.d.suchyta@gmail.com>
2016-02-20 07:25:19 -0800

    cleaning up Edison stuff

---
+++ site-setups/y1-edison-suchyta-setup.sh
@@ -0,0 +1,4 @@
+source /scratch1/scratchdirs/esuchyta/.local/setup/local-setup.sh
+local_setup --libs
+export Y1A1_DIR="/scratch1/scratchdirs/esuchyta/software/balrog_config/y1a1"
+export PATH="${Y1A1_DIR}/bin:${PATH}"
