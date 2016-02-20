52a7a4de88897eb6112c046e76216b8d25a1f90d
Eric Suchyta <eric.d.suchyta@gmail.com>
2016-02-20 07:54:28 -0800

    Edison stuff now more or less organized like I want

--- site-setups/Edison/y1-config.py
+++ site-setups/Edison/y1-config.py
@@ -2,32 +2,15 @@ import os
 import esutil
 
 
-def SVA1Setup(run, balrog):
-    run['release'] = 'sva1_coadd'
-    run['module_setup'] = 'balrog_sva1_setup'
-    run['outdir'] = os.path.join(os.environ['SCRATCH'],'BalrogScratch')
-
-    balrog['pyconfig'] = os.path.join(os.environ['BALROG_MPI'], 'pyconfig', 'slr2.py')
-    run['swarp-config'] = os.path.join(os.environ['ASTRO_CONFIG'], 'default.swarp') 
-    balrog['sexnnw'] = os.path.join(os.environ['ASTRO_CONFIG'],'sex.nnw')
-    balrog['sexconv'] = os.path.join(os.environ['ASTRO_CONFIG'], 'sex.conv')
-    balrog['sexparam'] = os.path.join(os.environ['ASTRO_CONFIG'], 'sex.param_diskonly')
-    balrog['nosimsexparam'] = os.path.join(os.environ['ASTRO_CONFIG'], 'sex.param_diskonly')
-    balrog['sexconfig'] = os.path.join(os.environ['ASTRO_CONFIG'], 'sex.config')
-
-    return run, balrog
-
-
 def Y1A1Setup(run, balrog, tiles):
-    #dir = os.environ['BALROG_CONFIG']
     dir = os.environ['Y1A1_DIR']
     tiles = esutil.io.read(os.path.join(dir, 'spt-y1a1-only-g70-grizY.fits'))['tilename']
+    run['pos'] = os.path.join(dir,'spt-y1a1-only-g70-grizY-pos')
 
     run['release'] = 'y1a1_coadd'
     run['db-columns'] = os.path.join(dir, 'y1a1_coadd_objects-columns.fits')
     balrog['pyconfig'] = os.path.join(dir, 'Y1-only.py')
     run['balrog'] = os.path.join(os.environ['LOCAL'], 'software', 'balrog.py')
-    #run['balrog'] = os.path.join(os.environ['BALROG_DIR'], 'balrog.py')
 
     run['swarp-config'] = os.path.join(dir, '20150806_default.swarp')
     balrog['sexnnw'] = os.path.join(dir, '20150806_sex.nnw')
@@ -39,41 +22,34 @@ def Y1A1Setup(run, balrog, tiles):
     return run, balrog, tiles
 
 
-
 # change the defaults if you want
 def CustomConfig(run, balrog, db, tiles):
     run, balrog, tiles = Y1A1Setup(run, balrog, tiles)
 
-    tstart = 135
-    tend = 185
+    tstart = 0
+    tend = 1
     tiles = tiles[tstart:tend]
+
+    run['nodes'] = 1
+    run['walltime'] = '00:30:00'
+    run['queue'] = 'debug'
+    run['runnum'] = 0     
+    
     run['npersubjob'] = 1
-    run['nodes'] = 50
-
-    balrog['ngal'] = 1000
-    run['tiletotal'] = 100000
-    run['indexstart'] = tstart * run['tiletotal']
-    run['runnum'] = 0 
-
-    run['walltime'] = '06:00:00'
-    run['queue'] = 'regular'
-
-    baseout = '/scratch1/scratchdirs/esuchyta'
-    #baseout = '/scratch3/scratchdirs/esuchyta/'
-    run['dbname'] = 'y1a1_sptn_01'
+    run['asdependency'] = True
+    
+    baseout = '/scratch3/scratchdirs/esuchyta/'
+    #baseout = '/scratch1/scratchdirs/esuchyta'
+    run['dbname'] = 'y1a1_test1'
     run['joblabel'] = '%i:%i' %(tstart, tend)
     run['jobdir'] = os.path.join(baseout, 'BalrogJobs')
     run['outdir'] = os.path.join(baseout, 'BalrogScratch')
 
-    run['DBoverwrite'] = False
+    balrog['ngal'] = 10
+    run['downsample'] = 50
+    run['runnum'] = 0 
+
+    run['DBoverwrite'] = True
     run['verifyindex'] = True
-
-
-    # If you want to run in my default way, you don't need to mess with these
-    run['ppn'] = 24
-    run['stripe'] = 2
-    run['asdependency'] = True
-    run['asarray'] = False
-    run['arraymax'] = None
 
     return run, balrog, db, tiles
