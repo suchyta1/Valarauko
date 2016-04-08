696a0e5685e37ba2083c27e10a45dee8cf565edd
Eric Suchyta <eric.d.suchyta@gmail.com>
2016-04-08 10:42:06 -0700

    minimal setups which will use python2.7

--- site-setups/shifter/y1a1/cori-minimal.sh
+++ site-setups/shifter/y1a1/cori-minimal.sh
@@ -1,2 +1,5 @@
-export STACK_BASE_DIR=/global/cscratch1/sd/zuntz/stack/
-export PYTHONPATH=$PYTHONPATH:${STACK_BASE_DIR}/usr/lib/python2.7/site-packages
+source /project/projectdirs/cmb/modules/hpcports_NERSC.sh
+hpcports gnu 
+module load hpcp
+module load python-hpcp
+export PYTHONPATH=/global/cscratch1/sd/zuntz/stack/usr/lib/python2.7/site-packages
---
+++ site-setups/shifter/y1a1/edison-minimal.sh
@@ -0,0 +1,5 @@
+export LOCAL="/scratch1/scratchdirs/esuchyta/.local"
+export CONDADIR="${LOCAL}/software/anaconda2"
+export PYTHONPATH="${LOCAL}/software/Balrog:${LOCAL}/lib/python2.7/site-packages"
+export PATH="${LOCAL}/bin:${CONDADIR}/bin:${PATH}"
+
