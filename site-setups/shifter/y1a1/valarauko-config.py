b35891a9312f702720089815d90d6501beb97f87
Eric Suchyta <eric.d.suchyta@gmail.com>
2016-04-08 07:24:10 -0700

    testing shifter on Cori

--- site-setups/shifter/y1a1/valarauko-config.py
+++ site-setups/shifter/y1a1/valarauko-config.py
@@ -7,28 +7,37 @@ BuildJob = imp.load_source('BuildJob', os.path.join(dir,'BuildJob.py'))
 
 
 def CustomConfig(run, balrog, db, tiles):
-    run = BuildJob.TrustEric(run, where='edison')
     run['shifter'] = 'esuchyta/knope:y1a1'
 
+    '''
+    run = BuildJob.TrustEric(run, where='edison')
     dir = '/scratch1/scratchdirs/esuchyta/software/balrog_config/y1a1/'
+    run['ppn'] = 24
+    #baseout = os.environ['SCRATCH']
+    baseout = '/scratch3/scratchdirs/esuchyta/'
+    '''
+
+    run = BuildJob.TrustEric(run, where='cori')
+    dir = '/global/cscratch1/sd/esuchyta/cori-software/balrog_config/y1a1/'
+    run['ppn'] = 32
+    baseout = os.environ['SCRATCH']
+
+
+    tiles = esutil.io.read(os.path.join(dir, 'spt-y1a1-only-g70-grizY.fits'))['tilename']
     run['pos'] = os.path.join(dir,'spt-y1a1-only-g70-grizY-pos-tile')
     balrog['catalog'] = os.path.join(dir, 'CMC_originalR_v1.fits')
     balrog['slrdir'] = dir
 
-    tiles = esutil.io.read(os.path.join(dir, 'spt-y1a1-only-g70-grizY.fits'))['tilename']
-
     tstart = 0
-    tend = 2
+    tend = 1
     tiles = tiles[tstart:tend]
-    run['nodes'] = 2
+    run['nodes'] = 1
 
     run['walltime'] = '00:30:00'
     run['queue'] = 'debug'
     run['runnum'] = 0     
     run['npersubjob'] = 1
     
-    #baseout = os.environ['SCRATCH']
-    baseout = '/scratch3/scratchdirs/esuchyta/'
     run['dbname'] = 'y1a1_stest'
     run['joblabel'] = '%i-%i' %(tstart, tend)
     run['jobdir'] = os.path.join(baseout, 'BalrogJobs')
