a0bcefe1b9f97206ca108cd231071a116d7cc7bc
Eric Suchyta <eric.d.suchyta@gmail.com>
2016-04-08 11:00:33 -0700

    cori minimal makes sense now

--- site-setups/shifter/y1a1/cori-minimal.sh
+++ site-setups/shifter/y1a1/cori-minimal.sh
@@ -2,4 +2,6 @@ source /project/projectdirs/cmb/modules/hpcports_NERSC.sh
 hpcports gnu 
 module load hpcp
 module load python-hpcp
-export PYTHONPATH=/global/cscratch1/sd/zuntz/stack/usr/lib/python2.7/site-packages
+module load numpy-hpcp
+module load astropy-hpcp
+export PYTHONPATH=/global/cscratch1/sd/zuntz/stack/usr/lib/python2.7/site-packages:${PYTHONPATH}
